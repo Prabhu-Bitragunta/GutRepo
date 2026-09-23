fd9861e4f098cacf9f5afd42d40ee5584a497402
Anil Bitragunta <bitraguntapandu11@gmail.com>
2026-09-23 18:02:37 +0530

    repush

--- MoveNegativeToFirst.java
+++ MoveNegativeToFirst.java
@@ -16,15 +16,20 @@ public class MoveNegativeToFirst {
 //				a[j]=temp;
 //			}
 //		}
-		int j=0;
-		for(int i=0;i<a.length;i++) {
-			if(a[i]<0) {
-				int temp=a[j];
-				a[j]=a[i];
-				a[i]=temp;
-				j++;
-			}
-		}
-		System.out.println(Arrays.toString(a));
+		
+//		int j=0;
+//		for(int i=0;i<a.length;i++) {
+//			if(a[i]<0) {
+//				int temp=a[j];
+//				a[j]=a[i];
+//				a[i]=temp;
+//				j++;
+//			}
+//		}
+//		System.out.println(Arrays.toString(a));
+		String s="amil";
+		System.out.println(System.identityHashCode(s));
+		s="kumar";
+		System.out.println(System.identityHashCode(s));
 	}
 }
